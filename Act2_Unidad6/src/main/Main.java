aa2fb196ce71eefee8da5b9a502557aab63fb7e3
Daniel Ramos Montoya <167306596+pidaisaca@users.noreply.github.com>
2025-04-29 17:42:34 +0100

    Actualización del main
    Propuesta de desarrollo para el combate

--- Act2_Unidad6/src/main/Main.java
+++ Act2_Unidad6/src/main/Main.java
@@ -1,9 +1,7 @@
 package main;
 
 import java.util.Scanner;
-
 import model.*;
-import interfaces.*;
 
 public class Main {
 
@@ -52,12 +50,21 @@ public class Main {
                     
                     System.out.println("Que personaje desea usar?");
                     juego.listarPersonajesDisponibles();
-                    //Seleccionar personaje 1
-                    //Borrar personaje 1 de lista de disponibles
-                    //Seleccionar personaje 2
-                    //Iniciar batalla (menu con opciones de ataque y defensa, además mostrar stats de ambos personajes o por turno)
-                    //Acaba batalla (cuando el personaje queda sin salud). Mostrar ganador.
-                    //Preguntar si se desea volver a jugar o salir
+                    /*Seleccionar personaje 1
+                    Listar personajes disponibles -> GetPersonaje de la lista -> Asignar personaje 1 a variable combatiente1 -> Sacar de la lista a personaje seleccionado -> Lista combatienes meter combatiente
+                    Listar personajes disponibles -> Seleccionar personaje 2 -> Asignar personaje 2 a variable combatiente2 -> Sacar de la lista a personaje seleccionado -> Lista combatientes meter combatiente
+                    Idealmente restaurar lista de personajes disponibles (ahora o al final de la batalla)
+                    Iniciar batalla (menu con opciones de ataque y defensa, además mostrar stats de ambos personajes o por turno)
+                        Bucle while con las stats de vida de ambos personajes, si alguno llega a 0 acaba batalla
+                            Definir turnos (turno del personaje 1, turno del personaje 2)
+                            Turno del personaje 1 -> Mostrar stats del personaje 1, preguntar si desea atacar o habilidad (Ej: guerrero.atacar())
+                                Mostrar stats del personaje 1 y menu. El menu se puede mostrar con un instance of guerrero, arquero, hechicero, asesino, mago desde la clase Personaje con un metodo abstracto que crearemos en cada clase como su menu
+                                Segun opcion elegida, restar vida al personaje 2 o establecer efecto en el personaje 1 -> (Ej con mago y guerrero: guerrero.atacar() = 20 por lo que mago.GetSalud() -= 20)
+                            Turno del personaje 2 -> Mostrar stats del personaje 2, preguntar si desea atacar o habilidad
+                                Segun opcion elegida, restar vida al personaje 1 o establecer efecto en el personaje 2 -> (Ej con mago y guerrero: mago.atacar() = 10 por lo que GUERRERO.GetSalud() -= 10)
+                                Repetir hasta que alguno de los personajes quede sin salud
+                    Mostrar ganador (Aquel cuyo valor de salud sea mayor, si ambos son iguales mostrar empate) -> Variable ganador = (comparar salud de ambos personajes y asignar el mayor -> con un if) // Sacar personaje de la lista de combatientes y mostrarla
+                    Preguntar si se desea volver al menu o salir (En caso de salir, opcion = 3 en el switch y en el otro dejar un break para volver al menu)*/
                     break;
 
                 case 3:
@@ -65,8 +72,52 @@ public class Main {
                     break;
             
                 default:
+                    System.out.println("Opcion no valida");
                     break;
             } 
             sc.close();   
     }
 }
+
+/* Posible diseño de las habilidades
+ *  
+ *  - Guerrero: 
+ *      - Cargar ataque: Aumenta el daño del siguiente ataque
+ *              1 turno de carga: daño fisico + x (o multiplicador)
+ *      - Defender: Reduce el daño del siguiente ataque en un %
+ *              1 turno de defensa: daño a recibir - x / valor de defensa sube en x
+ * 
+ *  - Arquero: 
+ *      - Volar: Alza el vuelo y aumenta la evasion contra ataques fisicos cuerpo a cuerpo
+ *              1 turno de vuelo: evasion fisica - boolean a comprobar en cada turno
+ *      - Disparar flechas: Gasta flechas para infligir daño fisico a distancia
+ *              5 cargas
+ *              1 turno de disparo: daño fisico + x
+ *      - Restaurar flechas: Recupera las flechas para volar y disparar en el turno siguiente
+ *              1 turno de restauracion: flechas + x
+ * 
+ *  - Hechicero:
+ *      - Invocar entidad: Efecto tipo daño en el tiempo (la entidad hace un ataque al principio del turno)
+ *              1 turno de invocacion: daño fisico extra (definir cant turnos)
+ *      - Lanzar hechizo: Lanza un hechizo basico desde la interfaz magica
+ *              1 turno de lanzar hechizo: daño magico + x
+ * 
+ *  - Asesino: 
+ *      - Ocultar: Otorga sigilo
+ *              Ocultar: aumentar evasión - valor de defensa + x / evitar golpe - boolean
+ *              cualquier accion lo elimina
+ *      - Atacar por la espalda: Daño critico garantizado, requiere sigilo
+ *              1 turno de atacar por la espalda: daño fisico * x (comprobar boolean de sigilo)
+ *              y quitar ocultar
+ *      - Curar: Lanza un hechizo curativo
+ *              1 turno de curar: salud + x
+ * 
+ *  - Mago: 
+ *      - Lanzar hechizo: Lanza un hechizo magico superior exclusivo del mago
+ *              1 turno de lanzar hechizo: daño magico + y
+ *              Coste de mana: x
+ *      - Regenerar mana: Regenera una gran cantidad de mana
+ *              1 turno de regenerar mana: mana + y
+ *      - Curar: Lanza un hechizo curativo
+ *              1 turno de curar: salud + x
+ */
